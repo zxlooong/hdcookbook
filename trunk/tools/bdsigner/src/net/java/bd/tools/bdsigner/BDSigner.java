9e3772ad6a0d556647150d392bb168a0a2d14613
chihiro_saito <chihiro_saito@e5771d8f-0841-e040-ffdb-cc069419ffcf>
2007-11-22 01:57:10 +0000

    Documentation change only.   Updated the usage example for the BDSigner tool, and added a missing copyright header.
    git-svn-id: https://svn.java.net/svn/hdcookbook~svn@109 e5771d8f-0841-e040-ffdb-cc069419ffcf

--- trunk/tools/bdsigner/src/net/java/bd/tools/bdsigner/BDSigner.java
+++ trunk/tools/bdsigner/src/net/java/bd/tools/bdsigner/BDSigner.java
@@ -1,14 +1,63 @@
+/*  
+ * Copyright (c) 2007, Sun Microsystems, Inc.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of Sun Microsystems nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ *  Note:  In order to comply with the binary form redistribution 
+ *         requirement in the above license, the licensee may include 
+ *         a URL reference to a copy of the required copyright notice, 
+ *         the list of conditions and the disclaimer in a human readable 
+ *         file with the binary form of the code that is subject to the
+ *         above license.  For example, such file could be put on a 
+ *         Blu-ray disc containing the binary form of the code or could 
+ *         be put in a JAR file that is broadcast via a digital television 
+ *         broadcast medium.  In any event, you must include in any end 
+ *         user licenses governing any code that includes the code subject 
+ *         to the above license (in source and/or binary form) a disclaimer 
+ *         that is at least as protective of Sun as the disclaimers in the 
+ *         above license.
+ * 
+ *         A copy of the required copyright notice, the list of conditions and
+ *         the disclaimer will be maintained at 
+ *         https://hdcookbook.dev.java.net/misc/license.html .
+ *         Thus, licensees may comply with the binary form redistribution
+ *         requirement with a text file that contains the following text:
+ * 
+ *             A copy of the license(s) governing this code is located
+ *             at https://hdcookbook.dev.java.net/misc/license.html
+ */
+
 package net.java.bd.tools.bdsigner;
 
 import java.io.File;
 import sun.security.tools.KeyTool;
 import sun.security.tools.JarSigner;
 
-/*
- * BDSigner.java
- * 
- * Created on Jul 17, 2007, 5:44:36 PM
- */
 
 /**
  * A class that wraps around jarsigner and keytool to perform bd-j required signing in one shot.
@@ -24,10 +73,11 @@ import sun.security.tools.JarSigner;
  * 
  * BJSigner syntax is : BDSigner [-debug] 8-digit-hex-organization-ID jar-files 
  * 
- * Example: java -cp $BDSIGNER_HOME/build/bdsigner.jar:$JDK_HOME/lib/tools.jar:$BDSIGNER_HOME/resource/bcprov-jdk15-137.jar net.java.bd.tools.bdsigner.BDSigner 56789abc 00000.jar 
+ * Example: java -Xbootclasspath/p:bdsigner-bootclasspath.jar -cp $BDSIGNER_HOME/build/bdsigner.jar:$JDK_HOME/lib/tools.jar:$BDSIGNER_HOME/resource/bcprov-jdk15-137.jar net.java.bd.tools.bdsigner.BDSigner 56789abc 00000.jar 
  * 
  * Make sure to put bdsigner.jar before tools.jar in the jdk distribution for the jre 
- * classpath so that the modified version of the sun.security.* classes in BDSigner this respository
+ * classpath, and to prepend bdsigner-bootclasspath.jar to the jre bootclasspath, so that 
+ * the modified version of the sun.security.* classes in this BDSigner respository
  * are used during the run.  bdprov-jdk15-137.jar is a bouncycastle distribution; a copy can be bound at "resources" dir.
  * 
  */
@@ -186,7 +236,7 @@ public class BDSigner {
 	 System.out.println("This is a tool to sign jar files according to the bd-j specification.\n");
 	 System.out.println("BDSigner Syntax:");
 	 System.out.println("net.java.bd.tools.bdsigner.BDSigner [-debug] 8-digit-hex-organization-ID jar-files");
-	 System.out.println("Example: java -cp bdsigner.jar:tools.jar:bcprov-jdk15-137.jar net.java.bd.tools.bdsigner.BDSigner 56789abc 00000.jar\n");
+	 System.out.println("Example: java -Xbootclasspath/p:bdsigner-bootclasspath.jar -cp bdsigner.jar:tools.jar:bcprov-jdk15-137.jar net.java.bd.tools.bdsigner.BDSigner 56789abc 00000.jar\n");
 	 System.out.println("\n==============================\n");
     }
 }
