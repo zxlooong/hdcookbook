3f2ce6e4125da2b908343d7b952a9d37766436a7
billf <billf@e5771d8f-0841-e040-ffdb-cc069419ffcf>
2008-01-23 12:01:39 +0000

    Updated the shell script that runs grinview on the menu xlet's show, and added a top-level build target to do the same thing, as a convenience.
    git-svn-id: https://svn.java.net/svn/hdcookbook~svn@181 e5771d8f-0841-e040-ffdb-cc069419ffcf

--- trunk/grin/scripts/shell/run_grinviewer.sh
+++ trunk/grin/scripts/shell/run_grinviewer.sh
@@ -2,14 +2,18 @@
 
 GRIN_BASE=`dirname $0`/../..
 CLASSES=$GRIN_BASE/build/grin_viewer/classes
+MENU_CLASSES=$GRIN_BASE/build/menu_tools
+EXTENSIONS_FACTORY=com.hdcookbook.bookmenu.menu.MenuExtensionsBuilderFactory
 
 case $1 in
     menu)
 	    HD_SRC=$GRIN_BASE/xlets/bookmenu/src/com/hdcookbook/bookmenu
 	    ASSETS=$HD_SRC/assets
 	    BG_IMG=$HD_SRC/menu/test_assets/MenuScreenBG_gray.png
-	    java -cp $CLASSES com.hdcookbook.grin.test.bigjdk.GrinView \
+	    java -cp $CLASSES:$MENU_CLASSES \
+	    	    com.hdcookbook.grin.test.bigjdk.GrinView \
 		    -asset_dir $ASSETS \
+		    -extensions_factory $EXTENSIONS_FACTORY \
 		    -background $BG_IMG \
 		    -fps 24 menu.txt
 	    ;;
