318bf4783f8be3f6c2489373ad0d6adb3eb741bd
jaya_h <jaya_h@e5771d8f-0841-e040-ffdb-cc069419ffcf>
2008-03-10 18:44:41 +0000

    Combined build targets, the reduced list of targets is as below:
    main     (Default)
    bd-tools (For building BDJO and security tools)
    all      (Full build with test execution; Sanity check before commit)
    javadoc  (Builds and deploys grin javadoc)
    clean
    
    Added an ant script: "run_grin_menu" for running menu xlet's show on GrinView.
    
    Reviewed by chihiro_saito and billf.
    
    
    git-svn-id: https://svn.java.net/svn/hdcookbook~svn@219 e5771d8f-0841-e040-ffdb-cc069419ffcf

--- trunk/grin/scripts/shell/build_book_menu.sh
+++ trunk/grin/scripts/shell/build_book_menu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cd ../../..
-ant build-grinview-menu
+cd ../ant/
+ant -f build_hdcookbook_xlets.xml build-menu-xlet
 if [[ $? != 0 ]] ; then
     exit 1;
 fi
--- trunk/grin/scripts/shell/make_grin_javadocs.sh
+++ trunk/grin/scripts/shell/make_grin_javadocs.sh
@@ -4,17 +4,13 @@ source vars.sh
 DEST=$HDC_BUILD_DIR/grin_javadoc
 
 cd $HDC_REPOSITORY
-ant build-grin-javadoc
+ant javadoc
 if [[ $? != 0 ]] ; then
     exit 1;
 fi
 
 
 echo ""
-echo "Built javadocs for all source files in the repository."
-echo "in $DEST"
-echo ""
-echo "By the way, before a putback to hdcookbook on dev.java.net, I do:"
-echo "    cp -r $DEST/* ../../../www/javadocs/grin"
-echo ""
+echo "Built javadocs for all source files in the repository:"
+echo "../../../www/javadocs/grin"
 
