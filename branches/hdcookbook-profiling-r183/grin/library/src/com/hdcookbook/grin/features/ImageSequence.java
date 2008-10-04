bb4596743a7b794008f87892e9a7a0e7ba9c4600
billf <billf@e5771d8f-0841-e040-ffdb-cc069419ffcf>
2008-10-04 00:28:13 +0000

    Oops - in optimization, didn't account for null images in an image_sequence.
    git-svn-id: https://svn.java.net/svn/hdcookbook~svn@377 e5771d8f-0841-e040-ffdb-cc069419ffcf

--- branches/hdcookbook-profiling-r183/grin/library/src/com/hdcookbook/grin/features/ImageSequence.java
+++ branches/hdcookbook-profiling-r183/grin/library/src/com/hdcookbook/grin/features/ImageSequence.java
@@ -330,7 +330,10 @@ public class ImageSequence extends Feature {
 	    return true;
 	}
 	for (int i = 0; i < num; i++) {
-	    images[i].unprepare();
+	    ManagedImage mi = images[i];
+	    if (mi != null) {
+		mi.unprepare();
+	    }
 	}
 	return false;
     }
