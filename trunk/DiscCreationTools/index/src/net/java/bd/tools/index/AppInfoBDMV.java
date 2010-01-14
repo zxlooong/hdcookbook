ba651a44fc98455091fe6cf746e9a08b0088f8a1
billf <billf@e5771d8f-0841-e040-ffdb-cc069419ffcf>
2010-01-14 18:34:29 +0000

    <no issue #>:  Added new entries to index.bdmv generator
    Added some new flags to index.bdmv to support 3D, as specified by 
    part 3-1 5.2.2.3 (V2.4d5).
    
    
    
    git-svn-id: https://svn.java.net/svn/hdcookbook~svn@588 e5771d8f-0841-e040-ffdb-cc069419ffcf

--- trunk/DiscCreationTools/index/src/net/java/bd/tools/index/AppInfoBDMV.java
+++ trunk/DiscCreationTools/index/src/net/java/bd/tools/index/AppInfoBDMV.java
@@ -65,40 +65,170 @@ import java.io.IOException;
 public class AppInfoBDMV {
     
     byte[] contentProviderData = new byte[32];
-    
-    /**
-     * Note: Commenting out to stop data represenatation to be in the xml file.
-     * According to the spec, this field is for the Content Provider's use and
-     * the field has no effect on BD-ROM player's behavior.
-     * When we encounter an use case for the field, it can be added back.
-     * 
-    public void setContentProviderData(byte[] data) {
-        if (data.length != 32) {
-            throw new RuntimeException("AppInfo data is not 32 bytes " + data.length);
-        }
-        this.contentProviderData = data;
+    InitialOutputModePreferenceType initialOutputModePreference
+    	= InitialOutputModePreferenceType.Mode2D;
+    boolean ssContentExistFlag = false;
+    VideoFormat videoFormat = VideoFormat.IGNORED;
+    FrameRate frameRate = FrameRate.IGNORED;
+  
+    public enum InitialOutputModePreferenceType {
+	Mode2D,		// 0
+	Mode3D;		// 1
     }
 
-    @XmlJavaTypeAdapter(HexStringBinaryAdapter.class)       
-    public byte[] getContentProviderData() {
-        return contentProviderData;
+    public enum VideoFormat {
+    		// This is almost the same as the video format defined in
+		// 5.4.4.3.2, but 5.2.2.3 specifies that a value of 0 is
+		// ignored by the player in this context.
+        IGNORED,
+        VIDEO_480i,
+        VIDEO_576i,
+        VIDEO_480p,
+        VIDEO_1080i,
+        VIDEO_720p,
+        VIDEO_1080p,
+        VIDEO_576p;
+
+        public int getEncoding() {
+            return (byte) ordinal();
+        }
+
+	public static VideoFormat getFromEncoding(int encoding) {
+            VideoFormat[] values = VideoFormat.values();
+            for (int i = 0; i < values.length; i++) {
+                if (values[i].ordinal() == encoding) {
+                    return values[i];
+                }
+            }
+	    assert false;
+	    return IGNORED;
+	}
+    }     
+   
+
+    public enum FrameRate {
+    		// This is almost the same as the frame rate defined in
+		// 5.4.4.3.2, but 5.2.2.3 specifies that a value of 0 is
+		// ignored by the player in this context.
+        IGNORED,
+        Hz_24000_1001,
+        Hz_24,
+        Hz_25,
+        Hz_30000_1001,
+        RESERVED_5,
+        Hz_50,
+        Mz_60000_1001;
+        
+        public int getEncoding() {
+            return ordinal();
+        }        
+
+	public static FrameRate getFromEncoding(int encoding) {
+            FrameRate[] frameRates = FrameRate.values();
+            for (int i = 0; i < frameRates.length; i++) {
+                if (frameRates[i].ordinal() == encoding) {
+                    return frameRates[i];
+                }
+            }
+	    assert false;
+	    return IGNORED;
+	}
     }
-    **/
+
+    public InitialOutputModePreferenceType getInitialOutputModePreference() {
+	return initialOutputModePreference;
+    }
+
+    public void setInitialOutputModePreference(
+    			InitialOutputModePreferenceType v) 
+    {
+	initialOutputModePreference = v;
+    }
+
+
+    public boolean getSSContentExistFlag() {
+	return ssContentExistFlag;
+    }
+
+    public void setSSContentExistFlag(boolean v) {
+	this.ssContentExistFlag = v;
+    }
+
+    public VideoFormat getVideoFormat() {
+	return videoFormat;
+    }
+
+    public void setVideoFormat(VideoFormat v) {
+	videoFormat = v;
+    }
+
+    public FrameRate getFrameRate() {
+	return frameRate;
+    }
+
+    public void setFrameRate(FrameRate v) {
+	frameRate = v;
+    }
+
+     // Note: Commenting out to stop data represenatation to be in the xml file.
+     // According to the spec, this field is for the Content Provider's use and
+     // the field has no effect on BD-ROM player's behavior.
+     // When we encounter a use case for the field, it can be added back.
+     // 
+    // public void setContentProviderData(byte[] data) {
+    //     if (data.length != 32) {
+    //         throw new RuntimeException("AppInfo data is not 32 bytes " + data.length);
+    //     }
+    //     this.contentProviderData = data;
+    // }
+
+    // @XmlJavaTypeAdapter(HexStringBinaryAdapter.class)       
+    // public byte[] getContentProviderData() {
+    //     return contentProviderData;
+    // }
     
     public void readObject(DataInputStream din) throws IOException {
-        
+       
+	// See 3-1 section 5.2.2.2
         // 32 bit length (should be constant, 34)
+	// 1 bit reserved
+	// 1 bit initial_output_mode_preference
+	// 1 bit SS content exist flag
+	// 5 bits reserved
+	// 4 bits video_format
+	// 4 bits frame_rate
         // 16 bit reserved
         // 8*32 user data
         
-        din.skipBytes(6); // reserved
+        din.skipBytes(4); // reserved
+	byte b = din.readByte();
+	if ((b & 0x40) != 0) {
+	    initialOutputModePreference =
+		InitialOutputModePreferenceType.Mode3D;
+	} else {
+	    initialOutputModePreference =
+		InitialOutputModePreferenceType.Mode2D;
+	}
+	ssContentExistFlag = (b & 0x20) != 0;
+	b = din.readByte();
+	videoFormat = VideoFormat.getFromEncoding(b >> 4);
+	frameRate = FrameRate.getFromEncoding(b & 0xf);
         din.read(contentProviderData);
     }
     
     public void writeObject(DataOutputStream dout) throws IOException {
-        byte[] reserved = new byte[2];
         dout.writeInt(34);
-        dout.write(reserved);
+	int i = 0;
+	if (initialOutputModePreference == InitialOutputModePreferenceType.Mode3D) {
+	    i |= 0x40;
+	}
+	if (ssContentExistFlag) {
+	    i |= 0x20;
+	}
+        dout.writeByte(i);
+	i = videoFormat.getEncoding() << 4;
+	i |= frameRate.getEncoding();
+        dout.writeByte(i);
         dout.write(contentProviderData);
     }
 
